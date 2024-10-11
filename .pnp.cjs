afff7f6fb0e2dd0cbc12334676f3288af0fa74db
Goldstack Project Builder <goldstack@pureleap.com>
2024-10-11 18:12:32 +0000

    Automated update of generated boilerplate by goldstack.party

--- .pnp.cjs
+++ .pnp.cjs
@@ -2646,10 +2646,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@goldstack/template-hetzner-vps", [\
-      ["npm:0.1.6", {\
-        "packageLocation": "./.yarn/cache/@goldstack-template-hetzner-vps-npm-0.1.6-ca38412ec2-cec97411f5.zip/node_modules/@goldstack/template-hetzner-vps/",\
+      ["npm:0.1.7", {\
+        "packageLocation": "./.yarn/cache/@goldstack-template-hetzner-vps-npm-0.1.7-51cce0500b-924028558c.zip/node_modules/@goldstack/template-hetzner-vps/",\
         "packageDependencies": [\
-          ["@goldstack/template-hetzner-vps", "npm:0.1.6"],\
+          ["@goldstack/template-hetzner-vps", "npm:0.1.7"],\
           ["@aws-sdk/credential-providers", "npm:3.645.0"],\
           ["@aws-sdk/types", "npm:3.609.0"],\
           ["@goldstack/infra", "npm:0.4.12"],\
@@ -2667,15 +2667,15 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@goldstack/template-hetzner-vps-cli", [\
-      ["npm:0.1.8", {\
-        "packageLocation": "./.yarn/cache/@goldstack-template-hetzner-vps-cli-npm-0.1.8-2e067789cb-cb983922ba.zip/node_modules/@goldstack/template-hetzner-vps-cli/",\
+      ["npm:0.1.9", {\
+        "packageLocation": "./.yarn/cache/@goldstack-template-hetzner-vps-cli-npm-0.1.9-2ccf2ef7c0-17825fdb22.zip/node_modules/@goldstack/template-hetzner-vps-cli/",\
         "packageDependencies": [\
-          ["@goldstack/template-hetzner-vps-cli", "npm:0.1.8"],\
+          ["@goldstack/template-hetzner-vps-cli", "npm:0.1.9"],\
           ["@aws-sdk/client-iam", "npm:3.645.0"],\
           ["@aws-sdk/client-s3", "npm:3.620.0"],\
           ["@aws-sdk/s3-request-presigner", "npm:3.645.0"],\
           ["@goldstack/infra-aws", "npm:0.4.20"],\
-          ["@goldstack/template-hetzner-vps", "npm:0.1.6"],\
+          ["@goldstack/template-hetzner-vps", "npm:0.1.7"],\
           ["@goldstack/utils-cli", "npm:0.3.10"],\
           ["@goldstack/utils-package", "npm:0.4.12"],\
           ["@goldstack/utils-package-config", "npm:0.4.12"],\
@@ -7428,8 +7428,8 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./packages/hetzner-vps-1/",\
         "packageDependencies": [\
           ["hetzner-vps-1", "workspace:packages/hetzner-vps-1"],\
-          ["@goldstack/template-hetzner-vps", "npm:0.1.6"],\
-          ["@goldstack/template-hetzner-vps-cli", "npm:0.1.8"],\
+          ["@goldstack/template-hetzner-vps", "npm:0.1.7"],\
+          ["@goldstack/template-hetzner-vps-cli", "npm:0.1.9"],\
           ["@goldstack/utils-git", "npm:0.2.9"],\
           ["@goldstack/utils-sh", "npm:0.5.11"],\
           ["@swc/core", "virtual:597045c1efe9d47bfbc154a42154773943e2d27cd2f7c6b6589d9ac6b9de7889616fd93ed452da5dec67eb71a3c2f696407bc4821b4030d0d0ae4764dcf8a120#npm:1.3.74"],\
